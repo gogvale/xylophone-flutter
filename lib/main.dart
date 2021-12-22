f75cdbeb335d36b5193059ee901bf92de6a54443
Gabriel Oliveira <gabriel.oliveira@axented.com>
2021-12-21 22:49:46 -0600

    Functional app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
@@ -8,9 +9,47 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: _notes(7),
+            ),
+          ),
         ),
       ),
     );
   }
+}
+
+void playSound(int i) {
+  final player = AudioCache();
+  player.play('note$i.wav');
+}
+
+List<SizedBox> _notes(int count) {
+  final colorList = [
+    Colors.redAccent,
+    Colors.orangeAccent,
+    Colors.yellow,
+    Colors.greenAccent,
+    Colors.teal,
+    Colors.blueAccent,
+    Colors.deepPurpleAccent
+  ];
+  return List.generate(
+    count,
+    (i) => SizedBox(
+      height:
+          // TODO: Get screen height properly
+          (WidgetsBinding.instance.window.physicalSize.height * 0.367) / count,
+      child: TextButton(
+        onPressed: () => playSound(i + 1),
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(colorList[i]),
+          padding: MaterialStateProperty.all(EdgeInsets.zero),
+        ),
+      ),
+    ),
+  ).toList(); // replace * with your rupee or use Icon instead
 }
